64c149b014d9b38559e6edfd0d6cb9e33ea3a534
Lucy Browning <lucybrowning@lucysmacbookair.lan>
2020-09-26 13:47:30 +0400

    new list using append

--- checkcard.py
+++ checkcard.py
@@ -1,17 +1,30 @@
-number = str(input("please input your credit card number with commas separating numbers"))
-number = number.split()
-print (number)
+# number = str(input("please input your credit card number with commas separating numbers"))
+# if number.isdigit():
+#     print("good, these are integers")
+# else:
+#     print("ERROR MESSAGE")
+# number = number.split(", ")
+# print(number)
+lst = []
+for i in range(16):
+    f = input("enter digit on credit card")
+    lst.append(f)
+    print(lst)
 
 
-def lengthoflist():
-    if len(number) > 16:
-        print("this is too big, not valid")
-    elif len(number) < 16:
-        print("this is too small, not valid")
-    else:
-        print("correct!")
 
-#
+
+
+
+# def lengthoflist():
+#     if len(number) > 16:
+#         print("this is too big, not valid")
+#     elif len(number) < 16:
+#         print("this is too small, not valid")
+#     else:
+#         print("correct!")
+
+
 # def valuecheck():
 #     try:
 #         val = int(number)
